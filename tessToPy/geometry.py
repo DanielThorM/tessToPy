f3ce14c66fe6726f6c81ae369eb4a08f05e3f434
Daniel Thor Morton <danieltm@ntnu.no>
2021-03-23 18:29:17 +0100

    Refactored tests

--- tessToPy/geometry.py
+++ tessToPy/geometry.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
+sys.path.insert(0, '../tessToPy/')
 from absdict import *
-#from mpl_toolkits.mplot3d import Axes3D
-#from mpl_toolkits.mplot3d.art3d import Poly3DCollection
-#from mpl_toolkits.mplot3d.art3d import Line3DCollection
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 class Vertex(object):
     def __init__(self, id_, coord):
         self.id_ = id_
@@ -41,6 +42,12 @@ class Vertex(object):
         if self.master == None:
             raise Exception('No master vertex')
         return np.sign(self.vector_to_master())
+
+    def plot(self, ax = None, color= 'k' , marker = 'o', **kwargs):
+        if  ax == None:
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(*self.coord, color=color, marker=marker, **kwargs)
 
 class Edge(object):
     def __init__(self, id_, verts):
@@ -89,7 +96,11 @@ class Edge(object):
         return periodicity
 
     def direction_relative_to_master(self):
-        return (self.vector()/self.master.vector())[0]
+        vect_sum = self.vector() + self.master.vector()
+        if np.all(np.isclose(vect_sum, np.array([0,0,0]))):
+            return -1
+        else:
+            return 1
 
     def x0(self):
         return self.verts[0].coord
@@ -112,6 +123,12 @@ class Edge(object):
         temp.master= self.master
         temp.part_of = self.part_of
         return temp
+
+    def plot(self, ax=None, color='k', **kwargs):
+        if ax == None:
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d')
+        ax.plot(*np.array([self.x0(), self.x1()]).swapaxes(0, 1), color=color, **kwargs)
 
 class Face(object):
     def __init__(self, id_, edges):
@@ -138,11 +155,11 @@ class Face(object):
             vert_list.extend(edge.verts)
         return list(set(vert_list))
 
-    def find_barycenter(self):
+    def barycenter(self):
         return np.array([vert.coord for vert in self.verts_in_face()]).mean(axis=0)
 
     def find_face_eq(self):
-        barycenter = self.find_barycenter()
+        barycenter = self.barycenter()
         vectors = []
         for edge in self.edges: #edgeID=self.edges[1]
             v1=edge.x0() - barycenter
@@ -156,7 +173,7 @@ class Face(object):
 
     def find_angle_deviation(self):
         vectors=[]
-        barycenter=self.find_barycenter()
+        barycenter=self.barycenter()
         for edge in self.edges:
             v1=edge.x0() - barycenter
             v2=edge.x1() - barycenter
@@ -199,98 +216,23 @@ class Face(object):
         temp.part_of = self.part_of
         return temp
 
-    def plot(self):
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
+    def plot(self, ax = None, color = 'k', normal_vector = False):
+        if ax == None:
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d')
         for edge in self.edges:
-            ax.plot(*np.array([edge.x0(), edge.x1()]).swapaxes(0, 1), color='k')
-        ax.scatter(*np.array([self.find_barycenter()]).swapaxes(0, 1), color='r')
-        ax.quiver(
-            *self.find_barycenter(),  # <-- starting point of vector
-            *self.find_face_eq()[1:]*edge.length(),  # <-- directions of vector
-            color='red', alpha=.8, lw=3)
+            coord = np.array([edge.x0(), edge.x1(), self.barycenter()])
+            triangle = Poly3DCollection(coord, alpha=0.4, facecolors=color)
+            ax.add_collection3d(triangle)
+        if normal_vector:
+            #ax.scatter(*np.array([self.barycenter()]).swapaxes(0, 1), color='r')
+            ax.quiver(
+                *self.barycenter(),  # <-- starting point of vector
+                *self.find_face_eq()[1:],  # <-- directions of vector
+                color='red', alpha=.8, lw=3)
 
-class FaceOld(object):
-    def __init__(self, edge_dict, id_, edges, state=0):
-        self.edge_dict = edge_dict
-        self.id_ = id_
-        self.edges = edges
-        self.state = state
-        self.master_to = []
-        self.slave_to = []
-        self.parents= []
-
-    def verts_in_face(self):
-        return list(set([self.edge_dict[edge].verts[0] for edge in self.edges]+[self.edge_dict[edge].verts[1] for edge in self.edges]))
-
-    def find_barycenter(self):
-        return np.array([self.edge_dict[self.edges[0]].vertex_dict[vert].coord for vert in self.verts_in_face()]).mean(axis=0)
-
-    def find_face_eq(self):
-        barycenter = self.find_barycenter()
-        vectors = []
-        for edge in self.edges: #edgeID=self.edges[1]
-            v1=self.edge_dict[edge].x0() - barycenter
-            v2=self.edge_dict[edge].x1() - barycenter
-            v3 = np.cross(v1, v2)
-            nv3 = v3 / np.linalg.norm(v3)
-            vectors.append(nv3)
-        averaged_vector = np.array(vectors).mean(axis=0)
-        face_eq_d = np.dot(averaged_vector, barycenter)
-        return [face_eq_d, averaged_vector[0], averaged_vector[1], averaged_vector[2]]
-
-    def find_angle_deviation(self, plot_face=False):
-        vectors=[]
-        barycenter=self.find_barycenter()
-        for edge in self.edges:
-            v1=self.edge_dict[edge].x0() - barycenter
-            v2=self.edge_dict[edge].x1() - barycenter
-            v3 = np.cross(v1, v2)
-            nv3 = v3 / np.linalg.norm(v3)
-            vectors.append(nv3)
-
-        mean_vector=np.array(vectors).mean(axis=0)
-        angles=[]
-        for i in range(len(vectors)):
-            j = i+1
-            if j ==len(vectors):
-                j=0
-            angles.append(np.arccos(
-                np.clip(np.dot(vectors[i], vectors[j]), -1.0, 1.0)))
-
-        baryangles = []
-        for i in range(len(vectors)):
-            baryangles.append(np.arccos(
-                np.clip(np.dot(vectors[i], mean_vector), -1.0, 1.0)))
-        max_angle=max(angles)
-        max_angle_ind = angles.index(max_angle)
-        max_bary_ind=baryangles[max_angle_ind:max_angle_ind+2].index(max(baryangles[max_angle_ind:max_angle_ind+2]))
-
-        return [self.edges[max_angle_ind+max_bary_ind], max_angle]
-
-    def plot_face(self):
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-        for i in range(len(self.edges)):
-            ax.plot(*np.array([self.edge_dict[self.edges[i]].x0(),self.edge_dict[self.edges[i]].x1()]).swapaxes(0,1))
-        ax.scatter(*np.array([self.edge_dict[self.edges[-1]].x0(),self.edge_dict[self.edges[-1]].x1()]).swapaxes(0,1))
-        ax.scatter(*np.array([self.find_barycenter()]).swapaxes(0,1))
-
-    def remove_edge(self, old_id):
-        target_ind = [abs(edge) for edge in self.edges].index(abs(old_id))
-        self.edges.pop(target_ind)
-
-    def replace_edge(self, old_id, new_id):
-        replaceInd = [abs(edge) for edge in self.edges].index(abs(old_id))
-        sign = np.sign(self.edges[replaceInd])
-        self.edges[replaceInd] = int(sign* new_id)
-
-    def reverse(self):
-        temp = FaceClass(edge_dict=self.edge_dict, id_=-self.id_, edges=[-1 * edge for edge in self.edges[::-1]], state = self.state)
-        temp.master_to = self.master_to
-        temp.slave_to  = self.slave_to
-        temp.parents = self.parents
-        return temp
+class Polyhedron(object):
+    pass
 
 class PolyhedronOld(object):
     def __init__(self, face_dict, id_, faces):
@@ -308,12 +250,12 @@ class PolyhedronOld(object):
 
 if __name__ == "__main__":
     coords = [[0,0,0], [1,0,0], [1,1,0], [0,1,0], [0,0,1],  [1,0,1], [1,1,1], [0,1,1]]
-    verts = absDict()
+    verts = absdict()
     for id_, coord in enumerate(coords):
         verts[id_] = Vertex(id_, coord)
     verts[0].add_slaves(list(verts.values())[1:])
     edge_ids = [[0, 1], [1,2], [2, 3], [3, 0], [0, 4], [1, 5], [2,6], [3,7], [4, 5], [5, 6], [6,7], [7,4]]
-    edges = absDict()
+    edges = absdict()
     for id_, edge in enumerate(edge_ids):
         edges[id_] = Edge(id_, [verts[i] for i in edge])
     edges[0].add_slaves([edges[2], edges[8], edges[10]])
@@ -325,7 +267,7 @@ if __name__ == "__main__":
              [3, 4, -11, -7],
              [0,1,2,3],
              [8,9,10,11]]
-    faces = absDict()
+    faces = absdict()
     for id_, face in enumerate(face_ids):
         faces[id_] = Face(id_, [edges[i] for i in face])
     faces[0].add_slave(faces[2])
--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -1,108 +1,104 @@
 import unittest
 import numpy as np
+#import sys, os
+#testdir = os.path.dirname(__file__)
+#srcdir = '../tessToPy/tessToPy'
+#sys.path.insert(0, os.path.abspath(os.path.join(testdir, srcdir)))
 import tessToPy.geometry as tg
 
 class TestVertex(unittest.TestCase):
+    def setUp(self):
+        self.coords = np.array([[0, 0, 0], [1.0, 1.1, 1.2]])
+        self.periodicity = np.array([-1, -1, -1])
+        self.vertices = [tg.Vertex(0, self.coords[0]), tg.Vertex(1, list(self.coords[1]))]
+        self.vertices[0].add_slave(self.vertices[1])
 
     def test_vector_to_master(self):
-        coords= np.array([[0,0,0],[1.0, 1.1, 1.2]])
-        a = tg.Vertex(0, coords[0])
-        b = tg.Vertex(1, list(coords[1]))
-        a.add_slave(b)
-        offset = coords[0]-coords[1]
-        self.assertIsNone(np.testing.assert_allclose(b.vector_to_master(), offset))
+        offset = self.coords[0]-self.coords[1]
+        vector_to_master = self.vertices[1].vector_to_master()
+        self.assertIsNone(np.testing.assert_allclose(vector_to_master, offset))
 
     def test_periodicity_to_master(self):
-        coords = np.array([[0, 0, 0], [1.0, 1.1, 1.2]])
-        a = tg.Vertex(0, list(coords[0]))
-        b = tg.Vertex(1, coords[1])
-        a.add_slave(b)
-        periodicity = np.array([-1, -1, -1])
-        self.assertIsNone(np.testing.assert_equal(b.periodicity_to_master(), periodicity))
+        periodicity_to_master = self.vertices[1].periodicity_to_master()
+        self.assertIsNone(np.testing.assert_equal(periodicity_to_master, self.periodicity))
 
 
     def test_no_master(self):
-        a = tg.Vertex(0, [0, 0, 0])
-        self.assertRaises(Exception, a.vector_to_master)
-        self.assertRaises(Exception, a.periodicity_to_master)
+        self.assertRaises(Exception, self.vertices[0].vector_to_master)
+        self.assertRaises(Exception, self.vertices[0].periodicity_to_master)
 
 
 class TestEdge(unittest.TestCase):
+    def setUp(self):
+        self.coords = np.array([[0, 0, 0], [0.0, 1.1, 1.2], [0.0, 2.1, 2.2], [0.0, 1.0, 1.0]])
+        self.vertices = [tg.Vertex(id_, coord) for id_, coord in enumerate(self.coords)]
+        self.edges = [tg.Edge(0, self.vertices[:2]), tg.Edge(0, self.vertices[2:4])]
+
     def test_xn(self):
-        coords= np.array([[0,0,0],[1.0, 1.1, 1.2]])
-        a = tg.Vertex(0, coords[0])
-        b = tg.Vertex(1, coords[1])
-        c = tg.Edge(0, [a, b])
-        self.assertIsNone(np.testing.assert_allclose(c.x0(), coords[0]))
-        self.assertIsNone(np.testing.assert_allclose(c.x1(), coords[1]))
-        self.assertIsNone(np.testing.assert_allclose(c.xm(), (coords[0]+coords[1]/2)))
+        edge = self.edges[0]
+        x0 = self.coords[0]
+        x1 = self.coords[1]
+        xm = (x0+x1)/2
+        self.assertIsNone(np.testing.assert_allclose(edge.x0(), x0))
+        self.assertIsNone(np.testing.assert_allclose(edge.x1(), x1))
+        self.assertIsNone(np.testing.assert_allclose(edge.xm(), xm))
 
     def test_vector(self):
-        coords = np.array([[0.0, 0, 0], [1.0, 1.1, 1.2]])
-        a = tg.Vertex(0, coords[0])
-        b = tg.Vertex(1, coords[1])
-        c = tg.Edge(0, [a, b])
-        self.assertIsNone(np.testing.assert_allclose(c.vector(), coords[1]))
+        edge = self.edges[0]
+        x0 = self.coords[0]
+        x1 = self.coords[1]
+        vector = x1-x0
+        self.assertIsNone(np.testing.assert_allclose(edge.vector(), vector))
 
     def test_length(self):
-        coords = np.array([[0.0, 0, 0], [1.0, 1.1, 1.2]])
-        a = tg.Vertex(0, coords[0])
-        b = tg.Vertex(1, coords[1])
-        c = tg.Edge(0, [a, b])
-        length = np.sqrt(coords[1,0]**2+coords[1,1]**2+coords[1,2]**2)
-        self.assertEqual(c.length(), length)
+        edge = self.edges[0]
+        x0 = self.coords[0]
+        x1 = self.coords[1]
+        vector = x1-x0
+        length = np.sqrt(vector[0]**2+vector[1]**2+vector[2]**2)
+        self.assertEqual(edge.length(), length)
 
     def test_vector_to_master(self):
-        coords = np.array([[0, 0, 0], [1.0, 1.1, 1.2], [2.0, 2.1, 2.2], [1.0, 1.0, 1.0]])
-        a, b, c, d = [tg.Vertex(id_, coord) for id_, coord in enumerate(coords)]
-        ea = tg.Edge(0, [a, b])
-        eb = tg.Edge(0, [c, d])
-        ea.add_slave(eb)
-        offset = np.array([-1.0, -1.0, -1.0])
-        self.assertIsNone(np.testing.assert_allclose(eb.vector_to_master(), offset))
+        edge_a = self.edges[0]
+        edge_b = self.edges[1]
+        edge_a.add_slave(edge_b)
+        vector_to_master = edge_a.xm() - edge_b.xm()
+        self.assertIsNone(np.testing.assert_allclose(edge_b.vector_to_master(), vector_to_master))
 
     def test_periodicity_to_master(self):
-        coords = np.array([[0, 0, 0], [1.0, 1.1, 1.2], [2.0, 2.1, 2.2], [1.0, 1.0, 1.0]])
-        a, b, c, d = [tg.Vertex(id_, coord) for id_, coord in enumerate(coords)]
-        ea = tg.Edge(0, [a, b])
-        eb = tg.Edge(0, [c, d])
-        ea.add_slave(eb)
-        periodicity = np.array([-1.0, -1.0, -1.0])
-        self.assertIsNone(np.testing.assert_allclose(eb.periodicity_to_master(), periodicity))
+        edge_a = self.edges[0]
+        edge_b = self.edges[1]
+        edge_a.add_slave(edge_b)
+        offset = edge_a.xm() - edge_b.xm()
+        periodicity_to_master = np.sign(offset)
+        self.assertIsNone(np.testing.assert_allclose(edge_b.periodicity_to_master(), periodicity_to_master))
 
     def test_direction_relative_to_master(self):
-        coords = np.array([[0, 0, 0], [1.0, 1.1, 1.2], [2.0, 2.1, 2.2], [1.0, 1.0, 1.0]])
-        a, b, c, d = [tg.Vertex(id_, coord) for id_, coord in enumerate(coords)]
-        ea = tg.Edge(0, [a, b])
-        eb = tg.Edge(0, [c, d])
-        ea.add_slave(eb)
-        self.assertEqual(eb.direction_relative_to_master(), -1)
-        coords = np.array([[0, 0, 0], [1.0, 1.1, 1.2], [1.0, 1.0, 1.0], [2.0, 2.1, 2.2]])
-        a, b, c, d = [tg.Vertex(id_, coord) for id_, coord in enumerate(coords)]
-        ea = tg.Edge(0, [a, b])
-        eb = tg.Edge(0, [c, d])
-        ea.add_slave(eb)
-        self.assertEqual(eb.direction_relative_to_master(), 1)
+        edge_a = self.edges[0]
+        edge_b = self.edges[1]
+        edge_a.add_slave(edge_b)
+        self.assertEqual(edge_b.direction_relative_to_master(), -1)
+        self.assertEqual(edge_b.reverse().direction_relative_to_master(), 1)
 
     def test_edge_reverse(self):
-        coords = np.array([[0, 0, 0], [1.0, 1.1, 1.2], [2.0, 2.1, 2.2], [1.0, 1.0, 1.0]])
-        a, b, c, d = [tg.Vertex(id_, coord) for id_, coord in enumerate(coords)]
-        ea = tg.Edge(0, [a, b])
-        eb = tg.Edge(0, [c, d])
-        ea.add_slave(eb)
-        eb_rev = eb.reverse()
-        ea_rev = ea.reverse()
-        self.assertEqual(eb_rev.direction_relative_to_master(), 1)
-        self.assertIsNone(np.testing.assert_allclose(ea.vector(), -1*ea_rev.vector()))
+        edge_a = self.edges[0]
+        edge_b = self.edges[1]
+        edge_a.add_slave(edge_b)
+        edge_a_rev = edge_a.reverse()
+        self.assertIsNone(np.testing.assert_allclose(edge_a.vector(), -1*edge_a_rev.vector()))
 
     def test_replace_vertex(self):
-        coords = np.array([[0, 0, 0], [1.0, 1.1, 1.2], [2.0, 2.1, 2.2], [1.0, 1.0, 1.0]])
-        a, b, c, d = [tg.Vertex(id_, coord) for id_, coord in enumerate(coords)]
-        ea = tg.Edge(0, [a, b])
-        ea.replace_vertex(a,c)
-        self.assertIsNone(np.testing.assert_allclose(ea.vector(), coords[1]-coords[2]))
+        edge_a = self.edges[0]
+        vector_org = edge_a.vector()
+        edge_a.replace_vertex(self.vertices[0],self.vertices[2])
+        vector = self.coords[1]-self.coords[2]
+        self.assertIsNone(np.testing.assert_allclose(edge_a.vector(), vector))
+        edge_a.replace_vertex(self.vertices[2], self.vertices[0])
+        self.assertIsNone(np.testing.assert_allclose(edge_a.vector(), vector_org))
 
 class TestFace(unittest.TestCase):
+
+
     def test_xn(self):
         coords= np.array([[0,0,0],[1.0, 1.1, 1.2]])
         a = tg.Vertex(0, coords[0])
@@ -176,6 +172,7 @@ class TestFace(unittest.TestCase):
         ea = tg.Edge(0, [a, b])
         ea.replace_vertex(a,c)
         self.assertIsNone(np.testing.assert_allclose(ea.vector(), coords[1]-coords[2]))
+
 
 if __name__ == '__main__':
     unittest.main()
